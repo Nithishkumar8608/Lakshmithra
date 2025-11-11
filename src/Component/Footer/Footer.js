929c8698a3916f9b6188d0777a94f1ff6016adeb
Nithishkumar8608 <gnks001@gmail.com>
2025-11-11 18:50:52 +0530

    footer changes

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,20 +1,52 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css";
+import logo from "../../assets/logo.png";
 
 const Footer = () => {
-    return (
-        <>
-        <footer className="bg-dark text-white py-4 mt-5">
+  return (
+    <>
+      <div className="Footer">
+        <div className="container">
+          <div className="row">
+            {/* Column 1 */}
+            <div className="col-md-6 col-lg-5 col-12 ft-1">
+              <img src={logo} alt="Lakshmithra Logo" width="50%" />
+              <p>
+                Lakshmithra (P) Ltd is committed to providing quality and trust
+                in every product and service we deliver.
+              </p>
+              
+            </div>
 
-          <div className="container text-center">
-            
-            <p className="mb-0">&copy; {new Date().getFullYear()} Lakshmithra. All rights reserved.</p>
+            {/* Column 2 */}
+            <div className="col-md-6 col-lg-3 col-12 ft-2">
+              <h5>Quick Links</h5>
+              <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About Us</a></li>
+                <li><a href="/products">Products</a></li>
+                <li><a href="/resources">Resources</a></li>
+                <li><a href="/contact">Contact</a></li>
+              </ul>
+            </div>
+
+            {/* Column 3 */}
+            <div className="col-md-6 col-lg-4 col-12 ft-3">
+              <h5>Contact</h5>
+              <p><i className="fa-solid fa-phone-volume"></i> +91 96006 84363</p>
+              <p><i className="fa-solid fa-envelope"></i> customercare@lakshmithra.com</p>
+              <p><i className="fa-solid fa-paper-plane"></i> 3/260, Moovendar Street, M.A.Nagar,
+                Redhills, Chennai - 600 052</p>
+            </div>
           </div>
-        </footer>
+        </div>
+      </div>
 
-    
+      <div className="footer-bottom">
+        <p>© 2025 Lakshmithra (P) Ltd. All Rights Reserved.</p>
+      </div>
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default Footer
+export default Footer;
